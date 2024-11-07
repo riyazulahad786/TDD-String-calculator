9912b13fc8cc9a90cb6f4976671925e057ecef42
riyazulahad786 <riyaz@gethelpnow.inm>
2024-11-07 15:11:02 +0530

    calculator.js

---
+++ src/calculator.js
@@ -0,0 +1,23 @@
+function calculator(numbers) {
+    if (numbers === '') return 0;
+
+
+    const delimiters = [',', '\n'];
+    const customDelimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    if (customDelimiterMatch) {
+        delimiters.push(customDelimiterMatch[1]);
+        numbers = numbers.split('\n').slice(1).join('\n');
+    }
+
+    const regex = new RegExp(`[${delimiters.join('')}]`);
+    const numberArray = numbers.split(regex).map(Number);
+
+    const negatives = numberArray.filter(n => n < 0);
+    if (negatives.length) {
+        throw new Error(`Negative numbers are not allowed: ${negatives.join(', ')}`);
+    }
+
+    return numberArray.reduce((sum, num) => sum + num, 1);
+}
+
+module.exports = calculator;
